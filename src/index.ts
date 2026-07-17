4834c7ba838f145dfa96c606bb571542fb9385fe
AmaanShikalgar <amaanshikalgar2003@gmail.com>
2026-07-17 17:02:55 +0530

    added new queries

--- src/index.ts
+++ src/index.ts
@@ -6,18 +6,38 @@ import { Client } from 'pg';
 const app = express();
 app.use(express.json());
 
-const pgClient = new Client(process.env.DB_URL)
+const pgClient = new Client({
+    connectionString: process.env.DB_URL
+});
 
-pgClient.connect();
+await pgClient.connect();
 
 app.post("/signup", async (req,res)=>{
     const username = req.body.username;
     const password = req.body.password;
     const email = req.body.email;
 
+    const city = req.body.city;
+    const country = req.body.country;
+    const street = req.body.street;
+    const pincode = req.body.pincode;
+
     try{
-        const insertQuery = `INSERT INTO users (username, email, password) VALUES ($1,$2,$3);`
-        await pgClient.query(insertQuery,[username,email,password]);
+                
+        const insertQuery = `INSERT INTO users (username, email, password) VALUES ($1,$2,$3) RETURNING id;`
+        
+        const addressInsertQuery = `INSERT INTO addresses (city,country,street,pincode,user_id) VALUES ($1,$2,$3,$4,$5);`
+        
+        await pgClient.query("BEGIN;")
+        
+        const response = await pgClient.query(insertQuery,[username,email,password]);
+        const userId = response.rows[0].id;
+
+        await new Promise(x=> setTimeout(x,5*1000));
+        const addressInsertResponse = await pgClient.query(addressInsertQuery,[city,country,street,pincode,userId])
+        
+        await pgClient.query("COMMIT;")
+
         res.json({
             message: "You have signed up"
         })
@@ -28,6 +48,20 @@ app.post("/signup", async (req,res)=>{
     }
 })
 
+app.get("/metadata",async (req,res)=>{
+    const id =req.query.id;
+
+    const query1 = `SELECT * FROM users WHERE id=$1`;
+    const response1 = await pgClient.query(query1,[id]);
+
+    const query2 = `SELECT * FROM addresses WHERE user_id=$1`;
+    const response2 = await pgClient.query(query2,[id]);
+
+    res.json({
+        user: response1.rows[0],
+        addresses: response2.rows[0]
+    })
+})
 app.listen(3000,function(){
     console.log("server is running")
 })
